19a4d874f98b2eea47e993e6a52bd735f83f6953
DespoinaKarli <128052927+DespoinaKarli@users.noreply.github.com>
2025-12-21 19:35:44 +0100

    remove preferences index.ts

--- frontend/src/components/frontend/src/components/preferences/index.ts
+++
@@ -1,9 +0,0 @@
-export { PreferencesEditor, getDefaultPreferences, type PreferencesEditorProps } from "./PreferencesEditor";
-export { IntegratedCalendar } from "./IntegratedCalendar";
-export { StatusCountdown } from "./StatusCountdown";
-export { ShiftCountsGrid } from "./ShiftCountsGrid";
-export { WeekdayPatterns } from "./WeekdayPatterns";
-export { ColleagueSelector } from "./ColleagueSelector";
-export { AdditionalNote } from "./AdditionalNote";
-export { WeekendRuleSection } from "./WeekendRuleSection";
-export * from "./types";
